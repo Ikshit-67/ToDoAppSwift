c93357985d93487e9577110c3a8c2d0b044f28ef
Ikshit C <109130289+Ikshit-67@users.noreply.github.com>
2023-02-10 13:59:27 +0530

    Initial Commit

---
+++ myToDO/myToDOApp.swift
@@ -0,0 +1,17 @@
+//
+//  myToDOApp.swift
+//  myToDO
+//
+//  Created by Ikshit Chaudhari on 10/02/23.
+//
+
+import SwiftUI
+
+@main
+struct myToDOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
